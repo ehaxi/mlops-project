8c215fa8ea179c72bee132183ca7752585dadfa0
ehaxi <nik.turkin.1991@gmail.com>
2025-04-21 11:07:25 +0300

    Logging models via MLflow is enabled

--- src/models/xgboost_clf.py
+++ src/models/xgboost_clf.py
@@ -1,9 +1,11 @@
+from datetime import datetime
 import pandas as pd
 from xgboost import XGBClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
 from base_model import BaseModel
+from pipeline import mlflow_utils, paths
 
-class LogRegClf(BaseModel):
+class XGBoostClf(BaseModel):
     def __init__(self, df: pd.DataFrame, path: str, splits: list, config=None):
         super().__init__(df, path, splits, config)
         
@@ -11,26 +13,45 @@ class LogRegClf(BaseModel):
 
         self.model = XGBClassifier(**params)
 
+        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+        self.model_name = f"{self.model.__class__.__name__}_{timestamp}"
+
     def cross_validation(self, splits):
         for x_train, x_test, y_train, y_test in splits:
             self.fit(x_train, y_train)
-            accuracy = self.evaluate(x_test, y_test)
-
-        return accuracy
+            self.evaluate(x_test, y_test)
 
     def fit(self, x_train, y_train):
-        self.model.fit(x_train, y_train)
+        with mlflow_utils.start_mlflow_run("XGBoost Experiment"):
+            mlflow_utils.log_params(self.config['models']['xgboost_clf'])
+            self.model.fit(x_train, y_train)
+            mlflow_utils.log_model(self.model)
 
     def predict(self, x_test):
         return self.model.predict(x_test)
 
     def evaluate(self, x_test, y_test):
         predictions = self.predict(x_test)
-        accuracy = accuracy_score(y_test, predictions)  # НУЖНО ИЗМЕНИТЬ НА МЕТРИКИ, КОТОРЫЕ БУДУТ ХРАНИТЬСЯ В .yaml
-        return accuracy
+
+        # Опционально изменить на метрики, которые будут храниться в .yaml
+        report = classification_report(y_test, predictions, output_dict=True)
+
+        flat_metrics = {}
+        for label, metrics in report.items():
+            if isinstance(metrics, dict):
+                for metric_name, value in metrics.items():
+                    flat_metrics[f"{label}_{metric_name}"] = value
+            else:
+                flat_metrics[label] = metrics
+
+        mlflow_utils.log_metrics(flat_metrics, model_name=self.model_name)
 
     def save_model(self):
-        pass
+        output_path_bybib = str(paths.project_root) + f'/trained_models/by_modelbib/{self.model_name}.cbm'
+        self.model.save_model(output_path_bybib)
+
+        # output_path_bymlflow = str(paths.project_root) + f'/trained_models/by_modelbib/{self.model_name}.cbm'
+        # mlflow_utils.log_artifact(output_path_bymlflow)
 
     def load_model(self):
         pass
